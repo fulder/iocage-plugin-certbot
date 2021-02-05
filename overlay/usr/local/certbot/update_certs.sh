266282e76571cad8d15aa08ad3c99ab856a74048
fulder <misad90@gmail.com>
2021-02-05 22:19:59 +0100

    Add log variable in update_certs script

--- overlay/usr/local/certbot/update_certs.sh
+++ overlay/usr/local/certbot/update_certs.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
+log = "/var/logs/iocage_certbot.log"
 cloudflare_ini = "/usr/local/certbot/.secrets/cloudflare.ini"
 
 cloudflare_api_key=$(sed 's/dns_cloudflare_api_token = //g' ${cloudflare_ini})
 
 if ! [ -z "$cloudflare_api_key" ]
 then
-    echo "Found cloudflare key" >> /var/logs/iocage_certbot.log
+    echo "Found cloudflare key" >> ${log}
     certbot certonly \
       --dns-cloudflare \
       --dns-cloudflare-credential ${cloudflare_ini} \
