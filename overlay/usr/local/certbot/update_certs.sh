0f0a6f5d85f8f561b7e72ba62ef6c187494227b3
fulder <misad90@gmail.com>
2021-02-05 22:18:37 +0100

    Start implementing update_certs script with cloudflare support

---
+++ overlay/usr/local/certbot/update_certs.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+cloudflare_ini = "/usr/local/certbot/.secrets/cloudflare.ini"
+
+cloudflare_api_key=$(sed 's/dns_cloudflare_api_token = //g' ${cloudflare_ini})
+
+if ! [ -z "$cloudflare_api_key" ]
+then
+    echo "Found cloudflare key" >> /var/logs/iocage_certbot.log
+    certbot certonly \
+      --dns-cloudflare \
+      --dns-cloudflare-credential ${cloudflare_ini} \
+      --dns-cloudflare-propagation-seconds 60 \
+      -d example.com
+fi
