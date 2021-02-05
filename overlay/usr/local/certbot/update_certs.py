cab9d2bd7e513c5403e15310855f14c35759d7d3
fulder <misad90@gmail.com>
2021-02-06 00:38:42 +0100

    Start implementing cloudflare function

--- overlay/usr/local/certbot/update_certs.py
+++ overlay/usr/local/certbot/update_certs.py
@@ -5,6 +5,7 @@ import yaml
 
 CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 CONFIG_LOCATION = os.path.join(CURRENT_DIR, "update_certs.yaml")
+LOG_LOCATION = os.path.join(CURRENT_DIR, "update_certs.log")
 
 logger = logging.getLogger("update_certs")
 
@@ -17,8 +18,11 @@ class UpdateCert:
     def main(self):
         self._load_config()
         self._setup_logger()
+        logger.info(f"Config loaded from: {CONFIG_LOCATION}")
 
+        self._cloudflare()
 
+        logger.info("Cert updater script completed")
 
     def _load_config(self):
         with open(CONFIG_LOCATION, "r") as f:
@@ -26,7 +30,6 @@ class UpdateCert:
 
     def _setup_logger(self):
         log_level = self.cfg["log_level"]
-        log_location = self.cfg["log_location"]
 
         formatter = logging.Formatter('%(asctime)s %(levelname)s %(name)s %(message)s')
 
@@ -36,10 +39,40 @@ class UpdateCert:
         sh.setLevel(log_level)
         logger.addHandler(sh)
 
-        fh = logging.FileHandler(log_location)
+        fh = logging.FileHandler(LOG_LOCATION)
         fh.setFormatter(formatter)
         fh.setLevel(log_level)
         logger.addHandler(fh)
+
+        logger.debug("Configured logger")
+
+    def _cloudflare(self):
+        domains = self.cfg["cloudflare"]["domains"]
+
+        if domains is None:
+            logger.info("No cloudflare domains found")
+            return
+
+        for domain in domains:
+            if "api_key" not in domain:
+                logger.error(f"Required 'api_key' property not found for domain: {domain}")
+            if "domain_name" not in domain:
+                logger.error(f"Required 'domain_name' property not found for domain: {domain}")
+
+            # create temporary cloudflare ini file
+            cloudflare_ini_path = os.path.join(CURRENT_DIR, "cloudflare_tmp.ini")
+            with open(cloudflare_ini_path, 'w') as f:
+                f.write(f"dns_cloudflare_api_token = {domain['api_key']}")
+
+            certbot_command = [
+                "certbot", "certonly",
+                "--dns-cloudflare",
+                "--dns-cloudflare-credentials", cloudflare_ini_path,
+                "-d", domain["domain_name"]
+            ]
+            logger.info(f"Running certbot command: {certbot_command}")
+
+            os.remove(cloudflare_ini_path)
 
 
 if __name__ == "__main__":
