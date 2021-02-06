1e6cb0b1db9981051abfdc7dca02ee38faa89475
fulder <misad90@gmail.com>
2021-02-06 01:49:54 +0100

    Add new_cron to crontab

--- post_install.sh
+++ post_install.sh
@@ -2,7 +2,9 @@
 
 pip install -r /usr/local/certbot/requirements.txt
 
-echo "0 0 * */2 *" python /usr/local/certbot/update_certs.py > new_cron
+echo "0 0 * */2 *" python3.7 /usr/local/certbot/update_certs.py > new_cron
+crontab new_cron
+rm new_cron
 
 echo "Installation successful. Plugin config file: /usr/local/certbot/update_certs.yaml"
 
