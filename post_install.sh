1b93d54f431f8a964eddc2dc4bbe07cfb48bbc58
fulder <misad90@gmail.com>
2021-02-06 01:00:39 +0100

    Remove old settings and add default cron in post_install

--- post_install.sh
+++ post_install.sh
@@ -2,5 +2,7 @@
 
 pip install -r requirements.txt
 
+echo "0 0 * */2 *" python /usr/local/certbot/update_certs.py > new_cron
+
 echo "Installation successful. Plugin config file: /usr/local/certbot/update_certs.yaml"
 
